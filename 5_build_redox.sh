92ef9180442f3f395149d1cd37fa0bf53e8e8f01
Jeremy Soller <jackpot51@gmail.com>
2016-02-28 20:04:02 -0700

    Cleanup build redox script

--- 5_build_redox.sh
+++ 5_build_redox.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 REDOX=~/Projects/redox/
 
-OLDPWD="$PWD"
-cd "$REDOX"
-make all
-cd "$OLDPWD"
+make -C "$REDOX" all
 
 cp -f "$REDOX/build/i386-unknown-redox/debug/harddrive.bin" work/redox.bin
 gzip -f work/redox.bin
-chmod 777 work/redox.bin.gz
 
