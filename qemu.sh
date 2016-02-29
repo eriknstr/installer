62e9b6cc2c29315a4e388bb128b768db02423551
Jeremy Soller <jackpot51@gmail.com>
2016-02-28 19:52:26 -0700

    KVM, no net

--- qemu.sh
+++ qemu.sh
@@ -5,5 +5,5 @@ then
     dd if=/dev/zero of=harddrive.bin bs=1M count=128
 fi
 
-qemu-system-x86_64 -hda harddrive.bin -cdrom minimal_linux_live.iso -serial mon:stdio
+qemu-system-x86_64 -enable-kvm -hda harddrive.bin -cdrom minimal_linux_live.iso -net none -serial mon:stdio -vga std
 
