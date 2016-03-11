e2a76a56bd086aa00d8e5913bb9098285a4ad58a
Jeremy Soller <jackpot51@gmail.com>
2016-03-11 08:47:47 -0700

    Change name of iso

--- 8_generate_iso.sh
+++ 8_generate_iso.sh
@@ -18,6 +18,6 @@ sed -i 's/mkisofs/genisoimage/g' arch/x86/boot/Makefile
 # Generate the ISO image with optimization for "parallel jobs" = "number of processors"
 make isoimage FDINITRD=../../rootfs.cpio.gz -j $(grep ^processor /proc/cpuinfo | wc -l)
 
-cp arch/x86/boot/image.iso ../../../minimal_linux_live.iso
+cp arch/x86/boot/image.iso ../../../redox-installer.iso
 
 cd ../../..
